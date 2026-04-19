3be3141251a5b059549d47bdb04c1c68c328614f
ToxicPotatooo <edwin.wong@edu.sait.ca>
2026-04-19 00:29:43 -0600

    Editing staff class

--- src/models/Staff.java
+++ src/models/Staff.java
@@ -2,22 +2,13 @@ package models;
 
 public class Staff extends Account {
 
-    private int staffId;
     private String position;
-
-    public Staff(int staffId, String lastName, String firstName, String phoneNum, String email, String hashPassword, String position) {
-        super(staffId, lastName, firstName, phoneNum, email, hashPassword);
-        this.staffId = staffId;
+    
+    public Staff(int accountId, String lastName, String firstName, 
+                 String phoneNum, String email, String hashPassword, 
+                 String position) {
+        super(accountId, lastName, firstName, phoneNum, email, hashPassword);
         this.position = position;
-    }
-
-    // Getters and Setters
-    public int getStaffId() {
-        return staffId;
-    }
-
-    public void setStaffId(int staffId) {
-        this.staffId = staffId;
     }
 
     public String getPosition() {
@@ -27,5 +18,5 @@ public class Staff extends Account {
     public void setPosition(String position) {
         this.position = position;
     }
-
+    
 }
