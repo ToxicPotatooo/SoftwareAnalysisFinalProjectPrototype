64d21d8b26cc7fcc087b45b4e935134ea7e199ed
Edwin Wong <edwin.wong@edu.sait.ca>
2026-04-19 14:38:41 -0600

    Update EquipmentManager.java

--- src/managers/EquipmentManager.java
+++ src/managers/EquipmentManager.java
@@ -1,5 +1,91 @@
 package managers;
 
-public class EquipmentManager {
+import models.Equipment;
+import utils.XlsxReader;
+import java.util.ArrayList;
 
+public class EquipmentManager {
+    
+    private ArrayList<Equipment> listOfEquipment;
+    private XlsxReader xlsxReader;
+    
+    public EquipmentManager() {
+        xlsxReader = new XlsxReader();
+        listOfEquipment = xlsxReader.loadEquipment();
+    }
+    
+    public boolean createNewEquipment(Equipment newEquipment) {
+        for (Equipment e : listOfEquipment) {
+            if (e.getEquipmentId() == newEquipment.getEquipmentId()) {
+                return false;
+            }
+        }
+        listOfEquipment.add(newEquipment);
+        xlsxReader.saveEquipment(listOfEquipment);
+        return true;
+    }
+    
+    public boolean updateEquipment(int targetEquipmentID, Equipment updatedEquipment) {
+        for (int i = 0; i < listOfEquipment.size(); i++) {
+            if (listOfEquipment.get(i).getEquipmentId() == targetEquipmentID) {
+                listOfEquipment.set(i, updatedEquipment);
+                xlsxReader.saveEquipment(listOfEquipment);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean deleteEquipment(int equipmentId) {
+        for (int i = 0; i < listOfEquipment.size(); i++) {
+            if (listOfEquipment.get(i).getEquipmentId() == equipmentId) {
+                listOfEquipment.remove(i);
+                xlsxReader.saveEquipment(listOfEquipment);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean reportEquipmentAsDamaged(int equipmentId) {
+        for (Equipment e : listOfEquipment) {
+            if (e.getEquipmentId() == equipmentId) {
+                return deleteEquipment(equipmentId);
+            }
+        }
+        return false;
+    }
+    
+    public ArrayList<Equipment> getListOfEquipment() {
+        return listOfEquipment;
+    }
+    
+    public Equipment getEquipmentById(int id) {
+        for (Equipment e : listOfEquipment) {
+            if (e.getEquipmentId() == id) {
+                return e;
+            }
+        }
+        return null;
+    }
+    
+    public ArrayList<Equipment> getEquipmentByCategory(String category) {
+        ArrayList<Equipment> result = new ArrayList<>();
+        for (Equipment e : listOfEquipment) {
+            if (e.getCategory().equalsIgnoreCase(category)) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
+    
+    public int getNextId() {
+        int maxId = 0;
+        for (Equipment e : listOfEquipment) {
+            if (e.getEquipmentId() > maxId) {
+                maxId = e.getEquipmentId();
+            }
+        }
+        return maxId + 1;
+    }
 }
